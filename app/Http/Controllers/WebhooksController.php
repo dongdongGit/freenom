51af4faad21a80956e3431bbdaf155f417a65c7b
dongdongGit <1697919363@qq.com>
2019-01-25 15:57:56 +0800

    test

--- app/Http/Controllers/WebhooksController.php
+++ app/Http/Controllers/WebhooksController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use Log;
 use Illuminate\Http\Request;
 
 class WebhooksController extends Controller
 {
     public function webhooks(Request $request)
     {
-        \Log::info($request->all());
+        $signature = 'sha1=' . hash_hmac('sha1', $request->getContent(), env('SECRET_WEBHOOKS'));
+
+        if ($signature !== $request->header('X-Hub-Signature')) {
+            Log::info('[error]验签失败');
+            return $this->error();
+        }
     }
 }
