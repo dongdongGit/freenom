3f018aee3f3533985c0ff4895b18170a45538f6a
dongdongGit <1697919363@qq.com>
2019-01-26 10:16:45 +0800

    test

--- app/Models/Domain.php
+++ app/Models/Domain.php
@@ -5,6 +5,7 @@ namespace App\Models;
 class Domain extends Model
 {
     protected $casts = [
+        'domain_id'          => 'integer',
         'user_id'            => 'integer',
         'enabled_auto_renew' => 'boolean',
         'register_time'      => 'date',
