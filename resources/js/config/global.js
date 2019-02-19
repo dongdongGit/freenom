fcc9a39c0b6e75ab2bdb198efc9bfd20d7f56671
dongdongGit <1697919363@qq.com>
2019-02-19 16:03:13 +0800

    fix

--- resources/js/config/global.js
+++ resources/js/config/global.js
@@ -1,6 +1,6 @@
 import env from ".././environments/environment.js";
 
-const baseUri = env.app_env == 'production' ? process.env.MIX_APP_URL : process.env.MIX_LOCAL_APP_URL;
+const baseUri = env.env == 'production' ? process.env.MIX_APP_URL : process.env.MIX_LOCAL_APP_URL;
 
 export default
 {
