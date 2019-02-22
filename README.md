c9824fae94216514c176aa50024965d36e40460f
dongdongGit <1697919363@qq.com>
2019-02-22 14:35:47 +0800

    adjust curl timeout

--- README.md
+++ README.md
@@ -2,10 +2,7 @@
 freenom auto renew
 
 ## Todo
- - job
  - lang
  - jwt token 
  - log
- - index
  - freenom service
- - webhooks 完善
--- config/freenom.php
+++ config/freenom.php
@@ -4,6 +4,8 @@ return [
     'username' => env('FREENOM_USERNAME'),
     'password' => env('FREENOM_PASSWORD'),
 
+    'timeout' => env('FREENOM_CURL_TIMEOUT', 15.0),
+
     'log' => [
         'level' => env('FREENOM_LOG_LEVEL', 'debug'),
         'file'  => env('FREENOM_LOG_FILE', storage_path('logs/freenom.log')),
