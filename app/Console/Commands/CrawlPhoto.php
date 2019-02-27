49b19cf9220e2e9b362088ec39f0cc177ac73f45
dongdongGit <1697919363@qq.com>
2019-02-27 23:47:51 +0800

    crawl photo

---
+++ app/Console/Commands/CrawlPhoto.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Storage;
+
+class CrawlPhoto extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'crawl:photo';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '爬取图片';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $client = new Client([
+            'timeout' => 10.0
+        ]);
+
+        $url = 'http://hyjf.5188cms.com/static/wechat/uploads/';
+
+        $response = $client->request(
+            'GET',
+            $url,
+            [
+                'headers' => [
+                    'Content-Type' => 'application/x-www-form-urlencoded',
+                    'User-Agent'   => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+                    'Accept'       => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'
+                ],
+                'allow_redirects' => true,
+            ]
+        );
+
+        $body = $response->getBody();
+
+        $doc = new \DOMDocument();
+        $page = mb_convert_encoding($body, 'HTML-ENTITIES', 'UTF-8');
+        @$doc->loadHTML($page);
+        $xpath = new \DOMXPath($doc);
+        $dom = $xpath->query('//*/td/a');
+        $imgs = [];
+
+        foreach ($dom as $key => $item) {
+            $imgs[] = sprintf($url . '%s', $item->attributes->getNamedItem('href')->nodeValue);
+        }
+
+        if (!empty($imgs)) {
+            Storage::put('app/crawl/photo.json', json_encode($imgs));
+        }
+    }
+}
