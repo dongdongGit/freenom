78615539978f3d6e76107054819dd12afcdd4703
dongdongGit <1697919363@qq.com>
2019-08-12 10:05:06 +0800

    fix: sentry send info

--- app/Console/Commands/ChronoSign.php
+++ app/Console/Commands/ChronoSign.php
@@ -5,6 +5,8 @@ namespace App\Console\Commands;
 use Exception;
 use Illuminate\Console\Command;
 use GuzzleHttp\Client;
+use Sentry\Severity;
+use Sentry\State\Scope;
 
 class ChronoSign extends Command
 {
@@ -53,23 +55,24 @@ class ChronoSign extends Command
                     'headers' => [
                         'Authorization' => 'JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImdhYXJhMTc1OUBnbWFpbC5jb20iLCJpZCI6ImRvbmdkb25nIiwidWlkIjoiNWQxOTdmOWQzZmMxYzkwMDEyMTc5MWMxIiwiaWF0IjoxNTYxOTUyMTg1LCJleHAiOjE1NjcxMzYxODUsImF1ZCI6Imh0dHBzOi8vd3d3LmNocm9uby5nZyIsImlzcyI6Imh0dHBzOi8vYXBpLmNocm9uby5nZyIsImp0aSI6IjUyOTA1NDE5MjNkYjQzNWViNzY1ZWRkZWNlZDc0NTAxIn0.jGDuLiUy_qoEbtHQmpBZ82_-yUTeTO6aMYzLzo_ZmEQ',
                         'Accept'        => 'application/json',
-                        'User-Agent'    => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100',
+                        'User-Agent'    => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100',
                     ],
                     'allow_redirects' => true,
                 ]
             );
 
-            info($response);
-            app('sentry')->captureMessage('fetch %s done', ['chrono sign'], [
-                'level' => 'info',
-                'extra' => [
-                    'status_code' => $response->getStatusCode(),
-                    'header'      => $response->getHeaders(),
-                    'body'        => $response->getBody(),
-                ]
-            ]);
+            $scope = new Scope();
+            $scope->setLevel(new Severity())
+                ->setTag('chrono', 'sign')
+                ->setExtra('headers', $response->getHeaders())
+                ->setExtra('body', $response->getbody());
+            app('sentry')->captureMessage(
+                'chrono sign done',
+                new Severity(),
+                $scope
+            );
         } catch (Exception $e) {
-            if (!in_array($e->response->statusCode, [200, 420]) && env('APP_ENV') == 'production' && app()->bound('sentry')) {
+            if (!empty($e->getResponse()) && !in_array($e->getResponse()->getStatusCode(), [200, 420]) && env('APP_ENV') == 'production' && app()->bound('sentry')) {
                 app('sentry')->captureException($e);
             }
         }
