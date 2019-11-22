f948f4e2754602a9b1682a8a237a82987865e5f6
dongdongGit <1697919363@qq.com>
2019-11-22 09:28:03 +0800

    fix lootboy offer

--- app/Console/Commands/LootboySign.php
+++ app/Console/Commands/LootboySign.php
@@ -4,7 +4,6 @@ namespace App\Console\Commands;
 
 use Exception;
 use Illuminate\Console\Command;
-use Illuminate\Support\Arr;
 use Sentry\Severity;
 
 class LootboySign extends Command
@@ -41,18 +40,18 @@ class LootboySign extends Command
     public function handle()
     {
         try {
-            $base_url = 'https://api.lootboy.de/v1/offers/';
+            $base_url = 'https://api.lootboy.de/v1/offers';
             $rand = mt_rand(3, 8);
             $header = [
                 'Authorization' => 'Bearer ' . env('LOOTBOY_TOKEN'),
                 'Accept'        => 'application/json',
                 'User-Agent'    => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/7{$rand}.0.3770.100",
             ];
-            $result = fetch($base_url . 'taken?lang=en', [], $header);
+            $result = fetch($base_url . '?lang=en', [], $header);
 
-            if (is_array($result) && !empty($offers = Arr::get($result, 'offers', []))) {
-                foreach ($offers as $offer) {
-                    $offer_result = fetch($base_url . $offer . '?lang=en', [], $header, 'PUT');
+            if (is_array($result)) {
+                foreach ($result as $offer) {
+                    $offer_result = fetch("{$base_url}/{$offer['id']}?lang=en", [], $header, 'PUT');
 
                     app('sentry')->configureScope(function ($scope) use ($offer_result) {
                         $scope->setLevel(new Severity())
@@ -61,7 +60,7 @@ class LootboySign extends Command
                             ->setLevel(Severity::info());
                     });
 
-                    app('sentry')->captureMessage("lootboy sign offer: {$offer}");
+                    app('sentry')->captureMessage("lootboy sign offer: {$offer['id']}");
                 }
             }
         } catch (Exception $e) {
